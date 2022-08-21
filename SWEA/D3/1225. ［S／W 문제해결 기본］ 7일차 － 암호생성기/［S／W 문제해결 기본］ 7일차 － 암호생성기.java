d60ada849b833e40dda2ea151d130dcccc284b9b
dam2 <106078444+dam2dam@users.noreply.github.com>
2022-08-21 19:57:30 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 137 ms, Memory: 24,680 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -1,44 +1,48 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
 
 public class Solution {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stringtokenizer;
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		//테스트 케이스 수
-		for (int t = 0; t < 10; t++) {
-			//테스트 케이스 번호 입력
-			int tc = scanner.nextInt();
-			//8개의 숫자 배열에 입력
-			int[] nums = new int[8];
-			for (int i = 0; i < 8; i++)
-				nums[i] = scanner.nextInt();
-
+		for (int tc = 1; tc <= 10; tc++) {
 			
-			while(nums[7] > 0) {
-				int decreasingNumber = 1;	//감소 해주는 수
-				int temp;	//첫번째 숫자를 임시 저장할 변수
-				
-				//한 사이클
-				for (int i = 0; i < 5; i++) {
-					temp = nums[0] - decreasingNumber++;	//첫번째 숫자를 감소 후
-					for (int j = 0; j < 7; j++)		//다른 숫자의 인덱스를 앞으로 당김
-						nums[j] = nums[j+1];
-					nums[7] = temp;		//감소한 첫번째 숫자 맨 뒤로 이동
-					
-					//감소한 숫자가 0보다 작으면 종료
-					if (nums[7] <= 0) {
-						nums[7] = 0;
-						break;
-					}
-				}
+			int T = Integer.parseInt(bufferedReader.readLine());
+			
+			Deque<Integer> deque = new LinkedList<>();
+			
+			stringtokenizer = new StringTokenizer(bufferedReader.readLine());
+			for (int i = 0; i < 8; i++) {
+				int input = Integer.parseInt(stringtokenizer.nextToken());
+				deque.offer(input);
 			}
 			
-			//출력
-			System.out.print("#"+tc+" ");
-			for (int n : nums)
-				System.out.print(n + " ");
+			int current;
+			int downNumber = 1;
+			while (deque.peekLast() != 0) {
+				if (downNumber > 5)
+					downNumber -= 5;
+				
+				current = deque.poll();
+				if(current - downNumber < 0) {
+					deque.offer(0);
+					break;
+				}
+				deque.offer(current - downNumber++);
+			}
+			
+			System.out.printf("#%d ", tc);
+			for(int i : deque)
+				System.out.print(i +" ");
 			System.out.println();
 		}
-		scanner.close();
+		bufferedReader.close();
 	}
+	
 }
