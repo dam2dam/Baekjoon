53b6ec2b5ac91668e5154f7901523041c6b2b03d
dam2 <106078444+dam2dam@users.noreply.github.com>
2022-08-21 18:34:50 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 146 ms, Memory: 31,160 KB -BaekjoonHub

--- SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
@@ -1,49 +1,56 @@
-import java.io.FileInputStream;
-import java.util.Scanner;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Solution {
- 
-    public static void main(String[] args) throws Exception {
-//      System.setIn(new FileInputStream("input.txt"));
-        Scanner scanner = new Scanner(System.in);
-        for (int t = 0; t < 10; t++) { //테스트 케이스
-            int tc = scanner.nextInt();
-             
-            //사다리 입력
-            int[][] ladder = new int[100][100]; 
-            for (int i = 0; i < 100; i++)
-                for (int j = 0; j < 100; j++)
-                    ladder[i][j] = scanner.nextInt();
- 
-            //현재좌표
-            int x = 0;
-            int y = 0;
-             
-            //도착점 찾기
-            for (int i = 0; i < 100; i++)
-                if(ladder[99][i] == 2) {
-                    x = 99;
-                    y = i;
-                }
- 
-            //방문체크
-            int[][] check = new int[100][100];
-             
-            //도착점부터 사다리를 거꾸로 올라가기
-            while(x >= 0) {
-                if(y>=1 && check[x][y-1]==0) //범위를 넘지 않고 방문하지 않은 경우
-                    while (y>=1 && ladder[x][y-1]==1) { //왼쪽에 통로가 있으면 가기
-                        y--;
-                        check[x][y] = 1;
-                    }
-                if(y<99 && check[x][y+1]==0) //범위를 넘지 않고 방문하지 않은 경우
-                    while (y<99 && ladder[x][y+1]==1) { //오른쪽에 통로가 있으면 가기
-                        y++;
-                        check[x][y] = 1;
-                    }
-                x--;
-            }
-            System.out.printf("#%d %d\n", tc, y);
-        }
-    }
+	
+	static int y, x;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stringtokenizer;
+
+		for (int tc = 1; tc <= 10; tc++) {
+			
+			int T = Integer.parseInt(bufferedReader.readLine());
+			
+			int[][] ladder = new int[100][100];
+			for (int i = 0; i < 100; i++) {
+				stringtokenizer = new StringTokenizer(bufferedReader.readLine());
+				for (int j = 0; j < 100; j++) {
+					ladder[i][j] = Integer.parseInt(stringtokenizer.nextToken());
+					if (ladder[i][j] == 2) {
+						y = i;
+						x = j;
+					}
+				}
+			}
+			boolean[][] visit = new boolean[100][100];
+			
+			while (y > 0) {
+
+				if(x-1 >= 0 && ladder[y][x-1] == 1 && visit[y][x-1] == false) {
+					visit[y][x-1] = true;
+					x--;
+					continue;
+				}
+				
+				if(x+1 < 100 && ladder[y][x+1] == 1 && visit[y][x+1] == false) {
+					visit[y][x+1] = true;
+					x++;
+					continue;
+				}
+				
+				if(ladder[y-1][x] == 1 && visit[y-1][x] == false) {
+					visit[y-1][x] = true;
+					y--;
+				}
+			}
+			
+			System.out.printf("#%d %d\n", tc, x);
+		}
+		bufferedReader.close();
+	}
+	
 }
