77e87cf4139169fe76d3a406a6ba848fed794826
dam2 <106078444+dam2dam@users.noreply.github.com>
2022-08-21 20:19:45 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 괄호 짝짓기, Time: 109 ms, Memory: 18,380 KB -BaekjoonHub

--- SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
+++ SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
@@ -1,49 +1,45 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Solution {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		//테스트 케이스 수
 		for (int tc = 1; tc <= 10; tc++) {
-			//테스트 케이스의 길이
-			int tcLength = scanner.nextInt();
-			//괄호 문자열을 입력받아 배열에 할당
-			char[] inputs = new char[tcLength];
-			String input = scanner.next();
-			for (int i = 0; i < inputs.length; i++)
-				inputs[i] = input.charAt(i);
 			
-			//유효성 여부	1:유효함, 0:유효하지 않음
-			int valid = 0;
-			//괄호를 하나씩 stack에 넣고 짝이 맞으면 pop
-			Stack<Character> brackets = new Stack<>();
-			for (int i = 0; i < inputs.length; i++) {
-				if (brackets.empty())
-					brackets.push(inputs[i]);
-
-				else if (brackets.peek() == '(' && inputs[i] == ')')
-					brackets.pop();
-
-				else if (brackets.peek() == '[' && inputs[i] == ']')
-					brackets.pop();
-
-				else if (brackets.peek() == '{' && inputs[i] == '}')
-					brackets.pop();
-
-				else if (brackets.peek() == '<' && inputs[i] == '>')
-					brackets.pop();
-
+			int T = Integer.parseInt(bufferedReader.readLine());
+			
+			Stack<Character> stack = new Stack<>();
+			
+			int valid = 1;
+			String input = bufferedReader.readLine();
+			char bracket;
+			for (int i = 0; i < T; i++) {
+				bracket = input.charAt(i);
+				if(stack.isEmpty() == true) {
+					stack.push(bracket);
+					continue;
+				}
+				if(stack.peek() == '(' && bracket == ')')
+					stack.pop();
+				else if(stack.peek() == '{' && bracket == '}')
+					stack.pop();
+				else if(stack.peek() == '[' && bracket == ']')
+					stack.pop();
+				else if(stack.peek() == '<' && bracket == '>')
+					stack.pop();
 				else
-					brackets.push(inputs[i]);
+					stack.push(bracket);
 			}
-			//짝이 모두 맞아 스택이 비어있으면 유효함
-			if (brackets.empty())
-				valid = 1;
-			//출력
+			if(stack.isEmpty() == false) 
+				valid = 0;
 			System.out.printf("#%d %d\n", tc, valid);
+			
 		}
-		scanner.close();
+		bufferedReader.close();
 	}
+	
 }
