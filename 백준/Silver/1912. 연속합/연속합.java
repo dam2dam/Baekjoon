6891378f125c5d721de0db2a0948abde7b1a3d0b
dam2 <106078444+dam2dam@users.noreply.github.com>
2023-07-06 22:48:06 +0900

    [Silver II] Title: 연속합, Time: 268 ms, Memory: 22832 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -13,14 +12,15 @@ public class Main {
 		int N = Integer.parseInt(bufferedReader.readLine());
 		int[] dp = new int[N + 1];
 		int currentNumber;
+		int max = Integer.MIN_VALUE;
 
 		stringTokenizer = new StringTokenizer(bufferedReader.readLine());
 		for (int i = 1; i <= N; i++) {
 			currentNumber = Integer.parseInt(stringTokenizer.nextToken());
 			dp[i] = Math.max(dp[i - 1] + currentNumber, currentNumber);
+			max = Math.max(max, dp[i]);
 		}
-		dp[0] = Integer.MIN_VALUE;
-		System.out.println(Arrays.stream(dp).max().getAsInt());
+		System.out.println(max);
 		bufferedReader.close();
 	}
 }
