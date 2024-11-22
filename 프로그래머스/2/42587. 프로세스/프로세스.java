f6d34b0328c86ba335303ce051d260d6f95322e7
dam2 <106078444+dam2dam@users.noreply.github.com>
2024-11-22 16:06:35 +0900

    [level 2] Title: 프로세스, Time: 2.52 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,32 +1,31 @@
 import java.util.ArrayDeque;
-import java.util.Collections;
-import java.util.PriorityQueue;
+import java.util.Arrays;
 import java.util.Queue;
 
 class Solution {
 
 	public int solution(int[] priorities, int location) {
 		int answer = 0;
-		// 우선순위 (내림차순)
-		PriorityQueue<Integer> prioritiesPQ = new PriorityQueue<>(Collections.reverseOrder());
 		// 실행 대기 큐
 		Queue<Process> processes = new ArrayDeque<>();
 		for (int i = 0; i < priorities.length; i++) {
-			int priority = priorities[i];
-			prioritiesPQ.offer(priority);
-			processes.offer(new Process(i, priority));
+			processes.offer(new Process(i, priorities[i]));
 		}
+        // 우선순위 정렬(오름차순)
+        Arrays.sort(priorities);
+        // 높은 우선순위의 존재여부가 중요하기 때문에 뒤에서부터 확인
+        int priorityIndex = priorities.length - 1;
 
 		int order = 0;
 		while (!processes.isEmpty()) {
 			Process process = processes.poll();
 			// 현재 프로세스의 우선순위보다 높은 우선순위를 가진 프로세스가 있다면
-			if (process.priority < prioritiesPQ.peek()) {
+			if (process.priority < priorities[priorityIndex]) {
 				// 프로세스를 다시 실행 대기 큐에 넣음
 				processes.offer(process);
 			} else {
 				// 프로세스 실행
-				prioritiesPQ.poll();
+                priorityIndex--;
 				order++;
 				// 현재 프로세스의 위치와 몇 번째로 실행되는지 알고싶은 프로세스의 위치가 같다면
 				if (process.location == location) {
