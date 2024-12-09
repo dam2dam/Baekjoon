01fe2baea138ff61fb23b29f086e9a14f566431c
dam2 <106078444+dam2dam@users.noreply.github.com>
2024-12-10 01:14:17 +0900

    [level 3] Title: 풍선 터트리기, Time: 42.22 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -3,11 +3,9 @@ class Solution {
     public int solution(int[] a) {
         int N = a.length;
         
-        int[] minOnLeft = new int[N];
-        findMinOnLeft(N, a, minOnLeft);
-        
+        int[] minOnLeft = new int[N];        
         int[] minOnRight = new int[N];
-        findMinOnRight(N, a, minOnRight);
+        findMin(N, a, minOnLeft, minOnRight);
         
         // 자신의 왼쪽과 오른쪽 모두 번호가 작은 풍선만 있다면 마지막까지 남을 수 없다.
         // 가장 왼쪽 풍선과 가장 오른쪽 풍선은 항상 마지막까지 남을 수 있으므로 2로 초기화 한다.
@@ -22,23 +20,18 @@ class Solution {
     }
     
     // 현재 위치보다 왼쪽에 있는 값 중, 최솟값을 기록한다.
-    static void findMinOnLeft(int N, int[] a, int[] minOnLeft) {
-        int min = a[0];
-        for (int i = 1; i < N; i++) {
-            minOnLeft[i] = min;
-            if (min > a[i]) {
-                min = a[i];
-            }
-        }
-    }
-    
     // 현재 위치보다 오른쪽에 있는 값 중, 최솟값을 기록한다.
-    static void findMinOnRight(int N, int[] a, int[] minOnRight) {
-        int min = a[N - 1];
-        for (int i = N - 2; i >= 0; i--) {
-            minOnRight[i] = min;
-            if (min > a[i]) {
-                min = a[i];
+    static void findMin(int N, int[] a, int[] minOnLeft, int[] minOnRight) {
+        int minLeft = a[0];
+        int minRight = a[N - 1];
+        for (int i = 1; i < N; i++) {
+            minOnLeft[i] = minLeft;
+            if (minLeft > a[i]) {
+                minLeft = a[i];
+            }
+            minOnRight[N - i - 1] = minRight;
+            if (minRight > a[N - i - 1]) {
+                minRight = a[N - i - 1];
             }
         }
     }
